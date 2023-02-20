b22ff92b3f2d132ac5e6d2f6595e42a65cc5209f
matheusrcruz <git.dev@outlook.com>
2023-02-20 16:00:37 -0300

    feature: versão final da pokeAPI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+//modules
+import { PagesModule } from './pages/pages.module';
+import {HttpClientModule} from '@angular/common/http';
+
+
+//components
 
 @NgModule({
   declarations: [
     AppComponent
   ],
+
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PagesModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
